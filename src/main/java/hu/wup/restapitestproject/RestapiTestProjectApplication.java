249162ede023da67772792cfb17f7d659a68aad2
hew220 <hew220@gmail.com>
2021-11-25 09:13:06 +0100

    initial project add

---
+++ src/main/java/hu/wup/restapitestproject/RestapiTestProjectApplication.java
@@ -0,0 +1,13 @@
+package hu.wup.restapitestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/wup/restapitestproject/RestapiTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.wup.restapitestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
